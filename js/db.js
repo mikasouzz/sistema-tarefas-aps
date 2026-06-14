4c5e62b53be70edad82eef20296b33bcf8a63605
alanmts-am <alanmts.am@gmail.com>
2026-06-14 15:33:46 -0300

    build: remove test environments

--- js/db.js
+++ js/db.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL  = 'https://tcegnkvivvjzbkealeno.supabase.co';
-const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjZWdua3ZpdnZqemJrZWFsZW5vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3ODk2OTQsImV4cCI6MjA5NjM2NTY5NH0.DowowhInp7nxRrrw6FLTHXq-Kn3ftQTFIlFuVBodJnU';
+const SUPABASE_URL  = '';
+const SUPABASE_ANON = '';
 
 export const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON);
