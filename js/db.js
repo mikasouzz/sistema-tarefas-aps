abd934a5a0f6f8ff408de9b19c566dd1ef28471e
alanmts-am <alanmts.am@gmail.com>
2026-06-14 15:43:50 -0300

    refactor: update environments

--- js/db.js
+++ js/db.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL  = '';
-const SUPABASE_ANON = '';
+const SUPABASE_URL  = 'https://unrkcldkqrncejxsssui.supabase.co';
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVucmtjbGRrcXJuY2VqeHNzc3VpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0NDkyNjAsImV4cCI6MjA5NzAyNTI2MH0.rWVIQfPIzYnU4OGQ-0J2aohjcX1FPH2GC7zsWX3ffn0';
 
 export const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON);
